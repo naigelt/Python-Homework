6b144b767d43635f53d565105ebae61ad677a4f2
Niko Tossavainen <nikotossavainen1999@gmail.com>
2022-08-22 14:53:06 +0300

    3 commit

---
+++ Teht_2.3.py
@@ -0,0 +1,8 @@
+korkeus = float(input("Anna suorakulman korkeus "))
+kanta = float(input("Anna suorakulman kannan pituus "))
+#laskut
+ala = kanta * korkeus
+piiri = (kanta * 2) + (korkeus * 2)
+#tulostukset
+print(f"ala = {ala}")
+print(f"piiri = {piiri}")
---
+++ Teht_2.4.py
@@ -0,0 +1,14 @@
+#syötteet
+luku_1 = int(input("Anna 1. kokonaisluku"))
+luku_2 = int(input("Anna 2. kokonaisluku"))
+luku_3 = int(input("Anna 2. kokonaisluku"))
+
+#laskut
+summa = luku_1 + luku_2 +luku_3
+tulo = luku_2 * luku_2 * luku_3
+ka = summa / 3
+
+#tulostukset
+print(summa)
+print(tulo)
+print(ka)
