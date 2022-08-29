dc2f673bd2780fb00436c2d1efbc1f3c30863054
Niko Tossavainen <nikotossavainen1999@gmail.com>
2022-08-29 13:19:58 +0300

    29.8 commit

---
+++ Tokan viikon tehtävät/Teht_3.1.py
@@ -0,0 +1,5 @@
+
+kuha = float(input("Anna kuhan pituus senttimetreinä "))
+full_lenght = 37
+if kuha<37:
+    print(f"Kuha on alamittainen laske se takaisin veteen, Kuhan on {full_lenght-kuha}cm alamittainen.")
