c34d688d274f9caa7ca0c97c23189b38c3c39aa8
Niko Tossavainen <nikotossavainen1999@gmail.com>
2022-09-01 20:51:01 +0300

    29.8 commit

--- Tokan viikon tehtävät/Teht_3.4.py
+++ Tokan viikon tehtävät/Teht_3.4.py
@@ -1,1 +1,8 @@
-print("Anna vuosiluku")
+year = int(input("Anna vuosiluku "))
+
+if year % 400 == 0.0 and year % 100 == 0.0:
+    print("Vuosi on karkausvuosi")
+elif  year % 4 == 0 and year % 100 != 0:
+    print(f"{year} on karkausvuosi")
+else:
+    print(f"{Year} ei ole karkausvuosi")
