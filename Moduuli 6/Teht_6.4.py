c74694f8fcbfeaf04273baf19dcc9c0dc1422c51
naigelt <nikotossavainen1999@gmail.com>
2022-09-07 17:29:55 +0300

    Tehty

---
+++ Moduuli 6/Teht_6.4.py
@@ -0,0 +1,9 @@
+list1 = [3, 5, 7, 8, 9, 1]
+
+def sumOfList(list, size):
+    if (size==0):
+        return 0
+    else:
+        return list[size-1] + sumOfList(list, size-1)
+total = sumOfList(list1, len(list1))
+print("Sum of all elements in the given list: ", total)
